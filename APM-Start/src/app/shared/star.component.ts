8d0269bdbfe5537075884e001d2d119c0371aaa8
Sofie Nordin <S.Nordin@ucas.ac.uk>
2021-11-08 15:11:25 +0000

    this is broken, help

--- APM-Start/src/app/shared/star.component.ts
+++ APM-Start/src/app/shared/star.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnChanges } from "@angular/core";
     styleUrls: ['./star.component.css']
 })
 export class StarComponent implements OnChanges {
-    @Input() rating: number = 0;
+    @Input() rating: number = 4;
     cropWidth: number = 75;
 
     ngOnChanges(): void {
